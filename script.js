176e6ef42fbd0c0fc3568d3a860e4840c6181e72
Glitch (glitch-hello-website) <none>
2021-05-29 16:15:07 +0000

    🚇🎁 Checkpoint ./script.js:20260864/275

--- script.js
+++ script.js
@@ -54,7 +54,9 @@ class Game {
     console.log("ticking");
     console.log(this.squares);
     // check for rules on the squares
-    
+    // copy this.squares- iterate through this.squares changing filled state in the copy. 
+    // then overwrite this.squares with the copy   
+    const squares = this.squares
     this.squares.forEach(square => {
       const filledNeighbors = square.getFilledNeighbors();
       console.log(square.filled, "filled");
