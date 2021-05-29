f2e639969d0e79f02f9864d9d5c82632fb28b7c5
Glitch (glitch-hello-website) <none>
2021-05-29 14:19:11 +0000

    👜👑 Checkpoint ./script.js:20260864/2514

--- script.js
+++ script.js
@@ -4,34 +4,53 @@ This is your site JavaScript code - you can add interactivity and carry out proc
 */
 // 500x500 canvas
 // 20 x 20 squares of 25 by 25 px
-const canvas = document.getElementById("myCanvas");
-const ctx = canvas.getContext("2d");
 const SQUARE_SIZE = 25;
 
 
 // Start w/ a seed
 // Tick every 2 seconds or something
 class Game {
-  constructor(){
-    
+  constructor(ctx){
+    this.ctx = ctx;
+    // new Square() based off the seed
   }
-  
+
   start() {
     // fillSquare(0, 0);
     // fillSquare(475, 475);
+    (new Square(0, 0, this.ctx)).fill();
+    console.log("in game", this.ctx);
   }
   
   tick() {
-    
+    // check for rules on the squares
   }
 }
 
 class Square {
+  construct(x, y, ctx) {
+    console.log("in square", ctx);
+    this.x = x;
+    this.y = y;
+    this.ctx = ctx;
+  }
   // properties: int x, int y, bool filled
   // array neighbors (of Squares)
   fill() {
-    ctx.fillRect(this.x, this.y, SQUARE_SIZE, SQUARE_SIZE);
+    this.ctx.fillRect(this.x, this.y, SQUARE_SIZE, SQUARE_SIZE);
+  }
+  
+  checkFilledNeighbors() {
+    // iterate over this.neighbors to count
+    // how many filled
+    
+    // return how many filled neighbors this square has
   }
 }
 
-(new Game()).start();
+window.addEventListener('load', () => {
+  const canvas = document.getElementById("myCanvas");
+  const ctx = canvas.getContext("2d");
+  console.log(ctx);
+  (new Game(ctx)).start();
+});
