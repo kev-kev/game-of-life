38dfeb7013443227638deed91739c21c1522b57e
Glitch (glitch-hello-website) <none>
2021-05-29 14:08:06 +0000

    🐘🎌 Checkpoint ./script.js:20260864/2691

--- script.js
+++ script.js
@@ -2,7 +2,36 @@
 This is your site JavaScript code - you can add interactivity and carry out processing
 - Initially the JS writes a message to the console, and rotates a button you can add from the README
 */
+// 500x500 canvas
+// 20 x 20 squares of 25 by 25 px
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+const SQUARE_SIZE = 25;
 
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
-ctx.fillRect(0, 0, 1, 1);
+
+// Start w/ a seed
+// Tick every 2 seconds or something
+class Game {
+  constructor(){
+    
+  }
+  
+  start() {
+    // fillSquare(0, 0);
+    // fillSquare(475, 475);
+  }
+  
+  tick() {
+    
+  }
+}
+
+class Square {
+  // properties: int x, int y, bool filled
+  // array neighbors (of Squares)
+  fill() {
+    ctx.fillRect(this.x, this.y, SQUARE_SIZE, SQUARE_SIZE);
+  }
+}
+
+(new Game()).start();
